5fd5024d304472ce8ebb8ee2e3ad5fe55339749b
Shimpei Iwamaru <gangan.education@gmail.com>
2019-10-25 20:56:41 +0900

    change button10

--- public/js/button10.js
+++ public/js/button10.js
@@ -1,3 +1,3 @@
 document.querySelector('.button10').onclick = function() {
-    alert('button10');
+    open( "https://twitter.com/pepabo?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor", "_blank" ) ;
 }
